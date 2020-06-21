2dc64135b9824ab5cc711538d0dbda9d94c4c499
BZIvanov <biser.z.ivanov@gmail.com>
2020-06-21 16:29:36 +0300

    import aliases config

---
+++ pages/portfolios/[id].js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PortfolioDetail = ({ query }) => {
+  const { id } = query;
+  return <h1>I am Detail Page with ID: {id}</h1>;
+};
+
+PortfolioDetail.getInitialProps = ({ query }) => {
+  return { query };
+};
+
+export default PortfolioDetail;
