512adce436632f313e785fc641762c834581ae7b
BZIvanov <ivanov.biser@gmx.com>
2020-06-26 20:36:16 +0300

    loading component and notifications

--- pages/login.js
+++ pages/login.js
@@ -1,11 +1,34 @@
+import { useEffect, useRef } from 'react';
 import LoginForm from '@/components/forms/login-form';
 import withApollo from '@/hoc/withApollo';
 import { useSignIn } from '@/apollo/actions';
+import { useRouter } from 'next/router';
 import Redirect from '@/components/shared/redirect';
 import BaseLayout from '@/layouts/BaseLayout';
+import messages from '@/variables/messages';
 
 const Login = () => {
+  const disposeId = useRef(null);
   const [signIn, { data, loading, error }] = useSignIn();
+  const router = useRouter();
+  const { message } = router.query;
+
+  const disposeMessage = () => {
+    router.replace('/login', '/login', { shallow: true });
+  };
+
+  useEffect(() => {
+    if (message) {
+      disposeId.current = setTimeout(() => {
+        disposeMessage();
+      }, 3000);
+    }
+
+    return () => {
+      clearTimeout(disposeId.current);
+    };
+  }, [message]);
+
   const errorMessage = (error) => {
     return (
       (error.graphQLErrors && error.graphQLErrors[0].message) ||
@@ -19,6 +42,11 @@ const Login = () => {
         <div className="row">
           <div className="col-md-5 mx-auto">
             <h1 className="page-title">Login</h1>
+            {message && (
+              <div className={`alert alert-${messages[message].status}`}>
+                {messages[message].value}
+              </div>
+            )}
             <LoginForm
               loading={loading}
               onSubmit={(signInData) => signIn({ variables: signInData })}
