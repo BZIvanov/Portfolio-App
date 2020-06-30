50fb197ae50cdcdeea0769df0376e4c32a1a8714
BZIvanov <ivanov.biser@gmx.com>
2020-06-30 08:58:24 +0300

    hide drop-down option for guest users

--- components/shared/navbar/index.js
+++ components/shared/navbar/index.js
@@ -55,12 +55,12 @@ const AppNavbar = () => {
               {user && (
                 <>
                   <span className="nav-link mr-2">Welcome {user.username}</span>
-                  <NavDropdown
-                    className="mr-2"
-                    title="Manage"
-                    id="basic-nav-dropdown"
-                  >
-                    {(user.role === 'admin' || user.role === 'instructor') && (
+                  {(user.role === 'admin' || user.role === 'instructor') && (
+                    <NavDropdown
+                      className="mr-2"
+                      title="Manage"
+                      id="basic-nav-dropdown"
+                    >
                       <>
                         <AppLink
                           href="/portfolios/new"
@@ -76,8 +76,8 @@ const AppNavbar = () => {
                           Dashboard
                         </AppLink>
                       </>
-                    )}
-                  </NavDropdown>
+                    </NavDropdown>
+                  )}
                   <AppLink href="/logout" className="nav-link btn btn-danger">
                     Sign Out
                   </AppLink>
